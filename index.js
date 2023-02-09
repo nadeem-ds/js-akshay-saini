8d11daf4e1c0022fe5d1722bcb1d68ab0622e68b
Nadeem Anwar <57771677+nadeem-ds@users.noreply.github.com>
2023-02-09 22:00:48 +0530

    hoisting js

---
+++ index.js
@@ -0,0 +1,22 @@
+getName3();
+console.log(a);
+console.log(getName);
+
+var a = 8; //undefind
+// let a = 8; //refrence error
+// const a = 8; //refrence error
+function getName() {
+  console.log("Learning js ");
+}
+
+var getName2 = () => {
+  console.log("Namaste js"); // it is not a functionon it behave like var
+};
+
+let getName3 = () => {
+  console.log("Namaste js"); // refrence error can not aacces before initilize
+};
+
+const getName4 = () => {
+  console.log("Namaste js"); // refrence error can not aacces before initilize
+};
